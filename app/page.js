6d4e9ce936d86f2ac7db635a005458d69b161711
Joshua Salazar <joshuasalazar.design@gmail.com>
2024-02-05 14:10:03 +0800

    Removed prev Company1, added npm sharp

--- app/page.js
+++ app/page.js
@@ -1,7 +1,6 @@
 import Header from "./components/Header"
 import Details from "./components/Details"
 import About from "./sections/about/About"
-import Company1 from "./components/Works/Company1/Company1"
 import BI from "./components/Works/boehringer/BI"
 
 export default function Home() {
@@ -13,10 +12,6 @@ export default function Home() {
       </div>
       <About />
       <BI />
-      {/* <div className="px-5">
-        <Header />
-        <Details />
-      </div> */}
     </main>
   )
 }
