14b71a64401d08a5d963e2d8c558efc60ea6bada
Joshua Salazar <joshuasalazar.design@gmail.com>
2024-02-06 20:34:10 +0800

    Nutri chunks

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@ import Header from "./components/Header"
 import Details from "./components/Details"
 import About from "./sections/about/About"
 import BI from "./components/Works/boehringer/BI"
+import NutriChunks from "./components/Works/nutrichunks/NutriChunks"
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       </div>
       <About />
       <BI />
+      <NutriChunks />
     </main>
   )
 }
