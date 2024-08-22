ed2314c6f1ecc18f43abaec1115a331a24c73d08
Joshua Salazar <joshuasalazar.design@gmail.com>
2024-08-22 10:42:30 +0800

    Add seline.so

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { Analytics } from "@vercel/analytics/react"
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -8,6 +9,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Script async src="https://cdn.seline.so/seline.js" />
         {children}
         <Analytics />
       </body>
